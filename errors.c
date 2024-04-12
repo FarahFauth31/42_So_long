d6ddebce9f6ef1302b6043d62836e2a5eece2fa0
Farah Fauth Puigdomenech <117769212+FarahFauth31@users.noreply.github.com>
2024-04-12 14:59:37 +0200

    Update errors.c

--- errors.c
+++ errors.c
@@ -6,39 +6,30 @@
 /*   By: ffauth-p <ffauth-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 18:19:50 by ffauth-p          #+#    #+#             */
-/*   Updated: 2024/04/09 20:07:52 by ffauth-p         ###   ########.fr       */
+/*   Updated: 2024/04/12 13:18:45 by ffauth-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	count_characters(t_game *game)
+int	find_invalid_characters(t_game *game, int i)
 {
-	int	i;
 	int	j;
 
-	i = 0;
-	game->C = 0;
-	game->P = 0;
-	game->E = 0;
 	while (game->map[i] != NULL)
 	{
 		j = 0;
 		while (game->map[i][j] != '\n' && game->map[i][j] != '\0')
 		{
-			if (game->map[i][j] == 'C')
-				game->C++;
-			if (game->map[i][j] == 'P')
-			{
-				fill_position(j, i, game->pos_P);
-				game->P++;
-			}
-			if (game->map[i][j] == 'E')
-				game->E++;
+			if (!(game->map[i][j] == 'C') && !(game->map[i][j] == 'P')
+				&& !(game->map[i][j] == 'E') && !(game->map[i][j] == '1')
+				&& !(game->map[i][j] == '0'))
+				return (ERROR);
 			j++;
 		}
 		i++;
 	}
+	return (EXIT_SUCCESS);
 }
 
 int	horizontal_limits(t_game *game)
@@ -86,20 +77,24 @@ int	vertical_limits(t_game *game)
 void	ft_reach(t_game *game, int x_pos, int y_pos)
 {
 	if (game->map[y_pos][x_pos] == '1' || y_pos < 0 || y_pos > game->height
-		|| x_pos < 0 || x_pos > game->width || game->map[y_pos][x_pos] == 'N')
+		|| x_pos < 0 || x_pos > game->width || game->map[y_pos][x_pos] == 'N'
+		|| game->map[y_pos][x_pos] == 'p' || game->map[y_pos][x_pos] == 'c'
+		|| game->map[y_pos][x_pos] == 'e')
 		return ;
 	if (game->map[y_pos][x_pos] == 'C')
 	{
-		game->reach_C = 1;
-		return ;
+		game->reach_c = 1;
+		game->map[y_pos][x_pos] = 'c';
 	}
 	if (game->map[y_pos][x_pos] == 'E')
 	{
-		game->reach_E = 1;
-		return ;
+		game->reach_e = 1;
+		game->map[y_pos][x_pos] = 'e';
 	}
-	if (game->map[y_pos][x_pos] != 'P')
+	if (game->map[y_pos][x_pos] == '0')
 		game->map[y_pos][x_pos] = 'N';
+	if (game->map[y_pos][x_pos] == 'P')
+		game->map[y_pos][x_pos] = 'p';
 	ft_reach(game, x_pos - 1, y_pos);
 	ft_reach(game, x_pos + 1, y_pos);
 	ft_reach(game, x_pos, y_pos - 1);
@@ -108,17 +103,17 @@ void	ft_reach(t_game *game, int x_pos, int y_pos)
 
 int	ft_errors(t_game *game)
 {
-	game->reach_E = 0;
-	game->reach_C = 0;
 	if (game->width == game->height || game->width < 3 || game->height < 3)
 		return (ERROR);
-	count_characters(game);
-	if (!(game->P == 1) || !(game->C == 1) || !(game->E == 1))
+	if (count_characters(game, 0) == ERROR || find_invalid_characters(game,
+			0) == ERROR)
+		return (ERROR);
+	if (!(game->p == 1) || game->c < 1 || !(game->e == 1))
 		return (ERROR);
 	if (horizontal_limits(game) == ERROR || vertical_limits(game) == ERROR)
 		return (ERROR);
-	ft_reach(game, game->pos_P[0], game->pos_P[1]);
-	if (game->reach_C == 0 || game->reach_E == 0)
+	ft_reach(game, game->pos_p_x, game->pos_p_y);
+	if (game->reach_c == 0 || game->reach_e == 0)
 		return (ERROR);
 	return (EXIT_SUCCESS);
 }
